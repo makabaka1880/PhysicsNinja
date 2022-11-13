76906d23ca95d73e0cdd8a0d9a900eea36846520
makabaka1880 <makabaka1880@outlook.com>
2022-11-13 20:41:06 +0800

    Added Shape Enum and random generator

---
+++ PhysicsNinja/ShapeType.swift
@@ -0,0 +1,43 @@
+//
+//  ShapeType.swift
+//  GeometryFighter
+//
+//  Created by SeanLi on 2022/11/13.
+//
+
+import Foundation
+
+enum ShapeType: Int {
+    case box        = 0
+    case sphere     = 1
+    case pyramid    = 2
+    case torus      = 3
+    case capsule    = 4
+    case cylinder   = 5
+    case cone       = 6
+    case tube       = 7
+    
+    static var random: Self {
+        let ran: Int = .random(in: 1...8)
+        return .init(rawValue: ran)!
+    }
+}
+
+
+extension BinaryInteger {
+    static func random() -> Self {
+        return self.init(arc4random())
+    }
+    static func random<T>(range: ClosedRange<T>) -> T where T: BinaryInteger {
+        let upper = T.init(range.upperBound)
+        let lower = T.init(range.lowerBound)
+        let random = T.init(
+            arc4random_uniform(
+                UInt32(
+                    upper - lower
+                )
+            ) / UInt32.max
+        ) + lower
+        return random
+    }
+}
