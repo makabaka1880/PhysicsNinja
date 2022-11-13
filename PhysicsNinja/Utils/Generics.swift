d26ae537fd062b3ab3c2ec2920b2d07c2ba2a372
makabaka1880 <makabaka1880@outlook.com>
2022-11-13 21:39:25 +0800

    deleted unused utils

--- PhysicsNinja/Utils/Generics.swift
+++
@@ -1,8 +0,0 @@
-
-import Foundation
-
-public func arc4random <T: ExpressibleByIntegerLiteral> (_ type: T.Type) -> T {
-  var r: T = 0
-  arc4random_buf(&r, Int(MemoryLayout<T>.size))
-  return r
-}
