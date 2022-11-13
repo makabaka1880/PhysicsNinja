d949f05eef9af52b2fbd4d5d86ae6f4d5b578c63
makabaka1880 <makabaka1880@outlook.com>
2022-11-13 21:37:28 +0800

    Added Utils

---
+++ PhysicsNinja/Utils/Double+Extensions.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+public extension Double {
+  public static func random(min: Double, max: Double) -> Double {
+    let r64 = Double(arc4random(UInt64.self)) / Double(UInt64.max)
+    return (r64 * (max - min)) + min
+  }
+}
+
